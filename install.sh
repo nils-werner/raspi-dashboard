59f96aef1163fdf5901549da20b895b4cbd36d7c
Nils Werner <nils.werner@gmail.com>
2014-02-01 10:14:22 +0100

    Chromium first attempts

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ netctl enable wlan0
 
 echo "Installing software"
 read -p "Press [Enter] key to continue..."
-pacman -S --noconfirm openbox midori ttf-freefont xorg-server xorg-xinit xorg-utils xorg-server-utils xf86-video-fbdev unclutter xdotool
+pacman -S --noconfirm openbox chromium ttf-freefont xorg-server xorg-xinit xorg-utils xorg-server-utils xf86-video-fbdev unclutter xdotool
 
 echo "Installing crontab"
 read -p "Press [Enter] key to continue..."
